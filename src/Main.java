c6e2b11e0e09c3a14e98557a1ca9a62c0f648260
theob34 <theo.bos@hotmail.fr>
2021-12-03 13:15:32 +0100

    Implémentation de la classe Main par Shivaree

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+	
+		
+		String test[][]=new String[2][7];
+		
+		Personnel personnel;
+
+		personnel=new Personnel("Roman","Loriot",22.08,"NOisy le Sec",null);
+		
+		
+		// personnel.modifDispo(test);
+		// System.out.println();
+		// personnel.modifDispo(test);
+		// personnel.toString(test);
+		// personnel.modifDispo(test);
+		// personnel.toString(test);
+	}
+
+}
